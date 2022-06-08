2d1455a92d403092ca6ade50afd6927e6739a56e
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-08 18:50:31 +0500

    change basepath

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-prixite-pages",
-  assetPrefix: "/nextjs-prixite-pages",
+  basePath: "",
+  assetPrefix: "",
 }
 
 module.exports = nextConfig
