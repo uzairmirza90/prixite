1166d24b105227459ee39a56cb6738658dfaa612
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-09 12:49:36 +0500

    remove image loader

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  // basePath: "/nextjs-prixite-pages",
-  // assetPrefix: "/nextjs-prixite-pages",
+  // images: {
+  //   loader: "akamai",
+  //   path: "",
+  // },
+  basePath: "/nextjs-prixite-pages",
+  assetPrefix: "/nextjs-prixite-pages",
 }
 
 module.exports = nextConfig
